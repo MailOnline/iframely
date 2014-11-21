98d1ad4f4ca22941c093b3405fad45f2937d1d52
Fabio Manera <fabio.manera@mailonline.co.uk>
2014-11-21 15:05:02 +0000

    only roll iframely.log if it exists

--- iframelyStart.sh
+++ iframelyStart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-NOW=$(date +"%m-%d-%Y")
-mv ~/.forever/iframely.log ~/.forever/iframely.log-$NOW
+[ -f ~/.forever/iframely.log ] && mv ~/.forever/iframely.log ~/.forever/iframely.log-`date +"%m-%d-%Y"`
 forever start -l iframely.log server.js
